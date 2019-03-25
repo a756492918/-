3c960c6c7be51ab817c919803aa5661f175876d3
a756492918 <756492918@qq.com>
2019-03-25 20:53:18 +0800

    vuex

---
+++ vuex/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue' //导入vue模块
+
+import Vuex from 'vuex' //导入vuex模块
+
+Vue.use(Vuex) //注册vuex中间件
+
+export default new Vuex.Store({     //暴露vuex
+    state : {
+        msg:1    //定义公共数据(vue全局共享)
+    }
+    mutations: {
+        add(state) {    //定义计算计算公共数据的方法
+            state.msg++   
+        }
+    }
+})
+
+// 在main.js中通过该方法引入
+import store from './(vuex的路劲)'
+
+// 在main.js中挂在store 这个vuex 
+
+
+//在全局组件中可以通过如下方法访问公共数据
+
+this.$store.state.msg  
+
+// 在全局组件中通过如下方法调用vuex方法，计算公共数据
+
+this.$store.commit('add')
