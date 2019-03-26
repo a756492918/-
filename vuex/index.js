75e97caaf0cb33cf1e1dd0ad1f0f6f344ea27649
a756492918 <756492918@qq.com>
2019-03-26 18:33:24 +0800

    vuex

--- vuex/index.js
+++ vuex/index.js
@@ -7,12 +7,18 @@ Vue.use(Vuex) //注册vuex中间件
 export default new Vuex.Store({     //暴露vuex
     state : {
         msg:1    //定义公共数据(vue全局共享)
-    }
+    },
     mutations: {
         add(state) {    //定义计算计算公共数据的方法
             state.msg++   
         }
+    },
+    getters:{
+        set(state){
+            reurn xxx
+        }
     }
+    
 })
 
 // 在main.js中通过该方法引入
@@ -28,3 +34,7 @@ this.$store.state.msg
 // 在全局组件中通过如下方法调用vuex方法，计算公共数据
 
 this.$store.commit('add')
+
+
+// getters内部的方法,可以直接通过如下方法调用得到返回值
+this.$store.getters.set
